a7eb7790667869da7b95e2cb99f3aad486e5ca5e
Daniel Paiva <daniel.00000853754@unicap.br>
2024-11-20 10:01:29 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SobreComponent },          // Rota padrão
+  { path: 'about', component: SobreComponent },    // Rota para o componente About
+  { path: 'contact', component: SobreComponent }
+];
