beb1b7577478ae507e3cd908ef402e6f0eb37d17
Daniel Paiva <daniel.00000853754@unicap.br>
2024-11-20 23:44:56 -0300

    rotas e ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { SobreComponent } from './components/sobre/sobre.component';
+import { GameComponent } from './components/game/game.component';
 
 export const routes: Routes = [
   { path: '', component: SobreComponent },          // Rota padrão
   { path: 'about', component: SobreComponent },    // Rota para o componente About
-  { path: 'contact', component: SobreComponent }
+  { path: 'proj', component: GameComponent }
 ];
